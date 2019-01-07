622fb211c69e8a8c8aa21dc9bb0807423673e4fc
Nagy Attyla <nagyattyla@Attilas-MBP.localdomain>
2019-01-07 21:44:51 +0200

    add load contacts

---
+++ public/javascripts/index.js
@@ -0,0 +1,36 @@
+
+function loadContacts(){
+    $.ajax('contacts.json').done(function(contacts){
+        console.log(contacts);
+        displayContacts(contacts);
+
+        
+    });
+        
+}
+
+function displayContacts(contacts){
+    var rows = contacts.map(function(contact) {
+        console.log('transform contact', contact);
+        return `<tr>
+          <td>${contact.firstName}</td>
+          <td>${contact.lastName}</td>
+          <td>${contact.phone}</td> 
+          <td><a href="contacts/delete?phone=${contact.phone}">X</a></td>       
+        
+        </tr>`;
+        
+    });
+    console.warn('rows', rows);
+    document.querySelector('tbody').innerHTML = rows.join('');
+
+    
+
+
+}
+
+
+loadContacts();
+
+
+
