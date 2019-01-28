96a18a03bd3ccb998c9013da8d5fa010c69f68bc
Nagy Attyla <nagyattyla@Attilas-MBP.localdomain>
2019-01-28 21:07:51 +0200

    search by all fields

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -3,8 +3,8 @@ var phoneToEdit = '';
 
 function loadContacts() {
     $.ajax('contacts.json').done(function (contacts) {
-        console.info('contacts loaded', contacts);
-        console.log(contacts);
+        // console.info('contacts loaded', contacts);
+        // console.log(contacts);
         window.globalContacts = contacts;
         displayContacts(contacts);
     });
@@ -19,25 +19,25 @@ function getNewRow() {
   
   </tr>`;
 }
-function saveContact(){
-   
+function saveContact() {
+
     var firstName = document.querySelector('input[name=firstName]').value;
     var lastName = $('input[name=lastName]').val();
     var phone = $('input[name=phone]').val();
     console.debug('saveContact...', firstName, lastName, phone);
-    
+
     var actionUrl = phoneToEdit ? 'contacts/update?phone=' + phoneToEdit : 'contacts/create';
-    
+
     // if(phoneToEdit){actionUrl = 'contacts/update'} else {actionUrl = 'contacts/create'};
-    
+
     $.post(actionUrl, {
         firstName, //shortcut from ES6 (key is the same as value variable name)
         lastName,
         phone: phone // ES5 (key= value)
-    }).done(function(response){
+    }).done(function (response) {
         console.warn('done create contact', response);
         phoneToEdit = '';
-        if(response.success){
+        if (response.success) {
             loadContacts();
         }
     });
@@ -47,7 +47,7 @@ function saveContact(){
 
 function displayContacts(contacts) {
     var rows = contacts.map(function (contact) {
-        console.log('transform contact', contact);
+        // console.log('transform contact', contact);
         return `<tr>
           <td>${contact.firstName}</td>
           <td>${contact.lastName}</td>
@@ -58,7 +58,7 @@ function displayContacts(contacts) {
             </td>       
         </tr>`;
     });
-    console.warn('rows', rows);
+    // console.warn('rows', rows);
 
     rows.push(getNewRow()); //simplified
 
@@ -68,29 +68,30 @@ function displayContacts(contacts) {
     document.querySelector('tbody').innerHTML = rows.join('');
 }
 
-function initEvents(){
+function initEvents() {
     // TODO use native click
-    $( "tbody" ).delegate( "a.edit", "click", function() {
+    $("tbody").delegate("a.edit", "click", function () {
         phoneToEdit = this.getAttribute('data-id');
 
-        var contact = globalContacts.find(function(contact){
+        var contact = globalContacts.find(function (contact) {
             return contact.phone == phoneToEdit;
         });
         console.warn(' edit', phoneToEdit, contact);
         document.querySelector('input[name=firstName]').value = contact.firstName;
         $('input[name=lastName]').val(contact.lastName);
-        $('input[name=phone]').val(contact.phone); 
+        $('input[name=phone]').val(contact.phone);
     });
 
     document.getElementById('search').addEventListener('input', doSearch);
 };
 
-function doSearch(){
-    var value = this.value;
-    
-    var filteredContacts = globalContacts.filter(function(contact){
-        console.log(contact.firstName, value);
-        return contact.firstName == value;
+function doSearch() {
+    var value = this.value.toLowerCase();
+
+    var filteredContacts = globalContacts.filter(function (contact) {
+        return contact.firstName.toLowerCase().includes(value) ||
+            contact.lastName.toLowerCase().includes(value) ||
+            contact.phone.toLowerCase().includes(value);
     });
 
     displayContacts(filteredContacts);
